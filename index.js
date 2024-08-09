317d1a2f1a921cc246e2aa3011da32c1d04c6783
“[tooba <toobaanwer44@gmail.com>
2024-08-09 19:06:23 +0500

    push my adventure game to github

---
+++ index.js
@@ -0,0 +1,48 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+class Player {
+    name;
+    fuel = 100;
+    constructor(name) {
+        this.name = name;
+    }
+    fuelDecrease() {
+        let fuel = this.fuel - 25;
+        this.fuel = fuel;
+    }
+}
+class Opponent {
+    name;
+    fuel = 100;
+    constructor(name) {
+        this.name = name;
+    }
+}
+let player = await inquirer.prompt([
+    {
+        name: "name",
+        type: "input",
+        message: "Please Enter your Name: "
+    }
+]);
+let opponent = await inquirer.prompt([
+    {
+        name: "select",
+        type: "list",
+        message: "Select your Opponent",
+        choices: ["Skeleton", "Alien", "Zombie"]
+    }
+]);
+let p1 = new Player(player.name);
+let o1 = new Opponent(opponent.select);
+do {
+    // Skeleton
+    if (opponent.select == "Skeleton") {
+    }
+    // Alien
+    if (opponent.select == "Alien") {
+    }
+    // Zombie
+    if (opponent.select == "Zombie") {
+    }
+} while (true);
